aa1ec023bb0fc52bc14472722692c8431cc22128
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-06-25 16:59:31 +0200

    style: implement Quiz Screen desktop header

--- src/screens/QuizScreen/QuizScreen.jsx
+++ src/screens/QuizScreen/QuizScreen.jsx
@@ -12,6 +12,10 @@ const mockQuestion = {
 };
 
 import ScreenLayout from '../../components/ScreenLayout';
+import logo from '../../assets/logo-desktop-on-light.svg';
+import styles from './QuizScreen.module.css';
+import TimerBar from '../../components/TimerBar';
+import clockIcon from '../../assets/clock-icon.svg';
 
 function QuizScreen({
   selectedTopic,
@@ -23,9 +27,38 @@ function QuizScreen({
   onFinish,
   onCancel,
 }) {
+  onSetTotalQuestions(20);
+
   return (
     <ScreenLayout>
-      <h1>Quiz Screen</h1>
+      <header className={styles.header}>
+        <div className={styles.rowTop}>
+          <img src={logo} alt="devquiz" className={styles.logo} />
+          <div className={styles.timerMeta}>
+            <img src={clockIcon} alt="Clock icon" className={styles.clockIcon} />
+            <p className={styles.timerText}>17:45</p>
+          </div>
+        </div>
+
+        <div className={styles.timerRow}>
+          <TimerBar />
+        </div>
+
+        <div className={styles.rowSecondary}>
+          <p className={styles.questionCounter}>Question 6 of {totalQuestions}</p>
+          <button className={styles.exitButton} onClick={onCancel}>
+            EXIT QUIZ <span aria-hidden="true">X</span>
+          </button>
+        </div>
+
+        <div className={styles.topicSummary}>
+          <img src={selectedTopic.image} alt="Topic icon" className={styles.topicIcon} />
+          <div className={styles.topicText}>
+            <p className={styles.topicName}>{selectedTopic.name}</p>
+            <p className={styles.topicDescription}>&gt; {selectedTopic.description}</p>
+          </div>
+        </div>
+      </header>
     </ScreenLayout>
   );
 }
