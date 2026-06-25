fefc30d6ea4e385878cd07f26008591d4451ade4
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-06-25 19:50:46 +0200

    feat: scroll to Start Quiz button on topic selection

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import styles from './HomeScreen.module.css';
 import logo from '../../assets/logo-desktop-on-light.svg';
 import QuizSelector from '../../components/QuizSelector';
@@ -6,6 +7,13 @@ import Button from '../../components/Button';
 import quizTopics from '../../data/quizTopics';
 
 function HomeScreen({ selectedTopic, onSelectTopic, onStart }) {
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    if (selectedTopic && buttonRef.current) {
+      buttonRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  }, [selectedTopic]);
   return (
     <ScreenLayout>
       <header className={styles.header}>
@@ -22,11 +30,13 @@ function HomeScreen({ selectedTopic, onSelectTopic, onStart }) {
         onSelectTopic={onSelectTopic}
       />
 
-      <Button onClick={onStart} disabled={!selectedTopic}>
-        Start Quiz <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-        </svg>
-      </Button>
+      <div ref={buttonRef}>
+        <Button onClick={onStart} disabled={!selectedTopic}>
+          Start Quiz <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+          </svg>
+        </Button>
+      </div>
     </ScreenLayout>
   );
 }
