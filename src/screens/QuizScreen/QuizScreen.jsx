db3fd2c6b16f0940025b3d7751e136ccc0a3da08
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-07-01 18:51:08 +0200

    refactor: add primary and secondary button variants

--- src/screens/QuizScreen/QuizScreen.jsx
+++ src/screens/QuizScreen/QuizScreen.jsx
@@ -72,11 +72,11 @@ function QuizScreen({
 
         <div className={styles.rowSecondary}>
           <p className={styles.questionCounter}>Question 6 of {totalQuestions}</p>
-          <button className={styles.exitButton} onClick={onCancel}>
+          <Button variant="secondary" onClick={onCancel}>
             EXIT QUIZ <span aria-hidden="true">        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
               <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
             </svg></span>
-          </button>
+          </Button>
         </div>
 
         <div className={styles.topicSummary}>
@@ -100,7 +100,7 @@ function QuizScreen({
         </div>
       )}
       <div ref={nextButtonRef}>
-        <Button disabled={!selectedAnswer} onClick={onFinish}>
+        <Button variant="primary" disabled={!selectedAnswer} onClick={onFinish}>
           Next Question <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
           </svg>
